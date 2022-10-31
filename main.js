3dcead902f3b45f54032c57ff3ad6c1abc9b4fc0
imark007 <imark@DESKTOP-8FD68H7.(none)>
2022-10-31 22:22:04 +0600

    initial commit

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
+console.log('Welcome to www.bdlearner.com');
 
 // please vist bdlearner.com
+
+
+
+
